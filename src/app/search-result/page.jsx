c2603c508970ff142c580065d0c60c5809e31ebe
ruhul-bdtask <ruhul@bdtask.net>
2025-02-03 15:06:19 +0600

    remove filterFlightsData

--- src/app/search-result/page.jsx
+++ src/app/search-result/page.jsx
@@ -12,7 +12,6 @@ import { notFound, useRouter } from "next/navigation";
 import { useEffect, useMemo, useRef, useState } from "react";
 import LoadingBar from "react-top-loading-bar";
 import useAirlineStore from "../../../stores/airlineStore";
-import { filterFlightsData } from "@/utils/filterFlightsData";
 export default function Page({ searchParams }) {
   const [loadingRevalidate, setLoadingRevalidate] = useState(false);
   const ref = useRef(null);
