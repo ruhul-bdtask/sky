e4d3085ca9acc08eefb1e47f62e351e0c2d4634a
ruhul-bdtask <ruhul@bdtask.net>
2025-02-08 20:42:16 +0600

    dynamic home page

--- src/components/blogs/BlogSection.jsx
+++ src/components/blogs/BlogSection.jsx
@@ -5,7 +5,6 @@ import SectionCover from "./SectionCover";
 import SectionHeader from "./SectionHeader";
 
 export default function BlogSection({ slug, sectionImage, heading }) {
-  console.log({ sectionImage });
   return (
     <div className="max-w-7xl mx-auto p-4 md:p-8">
       <SectionHeader
--- src/components/blogs/SectionCover.jsx
+++ src/components/blogs/SectionCover.jsx
@@ -1,21 +1,18 @@
 import blog1 from "@/public/images/blog1.png";
+import Image from "next/image";
 
 const SectionCover = ({ sectionImage }) => {
   function getImgUrl(name) {
-    return new URL(`@/public/images/blog1.png`, import.meta.url).href;
+    return new URL(`@/public/images/blog4.png`, import.meta.url).href;
   }
 
   return (
-    <div className="lg:col-span-1 md:p-10">
+    <div className="lg:col-span-1 md:p-4">
       <div className="rounded-lg overflow-hidden">
-        <img
+        <Image
           height={600}
           width={600}
-          src={
-            sectionImage
-              ? sectionImage
-              : new URL(`@/public/images/blog1.png`, import.meta.url).href
-          }
+          src={getImgUrl("blog4.png")}
           alt="Tropical destination"
           className="w-full h-full object-cover"
         />
